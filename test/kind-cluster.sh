c1476fdd8e8bbdc42a286f85787292c581529ab3
Haos Games <haosgames@protonmailcom>
2025-08-11 18:11:20 +0200

    fix: helm chart and test

--- test/kind-cluster.sh
+++ test/kind-cluster.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
 
-kind delete cluster cnpg-dev
+kind delete cluster -n cnpg-dev
 kind create cluster -n cnpg-dev
 kubectl apply --server-side -f \
   https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.26/releases/cnpg-1.26.1.yaml
-kubectl apply -f pg-cluster.yaml
+kubectl apply -f test/pg-cluster.yaml
+kubectl apply -f test/restic-secret.yaml
+helm upgrade --install -f test/helm-values.yaml backup ./charts/backup
